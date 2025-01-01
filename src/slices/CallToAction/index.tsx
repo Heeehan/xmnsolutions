7afef6b34ee64cfe313f2b5f686ab3ab9a632cb9
Heeehan <ianmonash10@gmail.com>
2025-01-02 05:45:53 +1100

    took logo out

--- src/slices/CallToAction/index.tsx
+++ src/slices/CallToAction/index.tsx
@@ -1,6 +1,5 @@
 import { Content } from "@prismicio/client";
 import { PrismicText, SliceComponentProps } from "@prismicio/react";
-import PlainLogo from "./plainlogo";
 import Bounded from "@/components/Bounded";
 import ButtonLink from "@/components/ButtonLink";
 
@@ -22,7 +21,6 @@ const CallToAction = ({ slice }: CallToActionProps): JSX.Element => {
       <div className="glow absolute -z-10 aspect-square w-full max-w-sm rounded-full bg-blue-500/50 blur-[160px] filter" />
 
       <div className="glass-container rounded-lg bg-gradient-to-b from-slate-800 to-slate-900 p-4 md:rounded-xl">
-        <PlainLogo />
       </div>
 
       <div className="mt-8 max-w-xl text-balance text-5xl">
