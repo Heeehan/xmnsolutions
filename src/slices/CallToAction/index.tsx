a91647b5e6c4c2c4b9b1e4937b2c1747f6423c08
Heeehan <ianmonash10@gmail.com>
2025-01-02 05:43:38 +1100

    I fixed the plain Logo

--- src/slices/CallToAction/index.tsx
+++ src/slices/CallToAction/index.tsx
@@ -1,8 +1,8 @@
 import { Content } from "@prismicio/client";
 import { PrismicText, SliceComponentProps } from "@prismicio/react";
+import PlainLogo from "./plainlogo";
 import Bounded from "@/components/Bounded";
 import ButtonLink from "@/components/ButtonLink";
-import PlainLogo from "./plainlogo"
 
 /**
  * Props for `CallToAction`.
@@ -19,17 +19,19 @@ const CallToAction = ({ slice }: CallToActionProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="relative py-32 text-center font-medium md:py-40"
     >
-      <div className="glow absolute -z-10 aspect-square w-full max-w-sm rounded-full bg-blue-500/50 blur-[160px] filter"/>
-    <div className="glass rounded-lg bg-gradient-to-b from-slate-800 to-slate-900 p-4 md:rounded-xl">
-      <PlainLogo />
-    </div>
-    <div className="mt-8 max-w-xl text-5xl">
-      <PrismicText field={slice.primary.heading} />
-    </div>
-    <ButtonLink field={slice.primary.button_link} className="mt-6">
-      {slice.primary.button_text}
-    </ButtonLink>
+      <div className="glow absolute -z-10 aspect-square w-full max-w-sm rounded-full bg-blue-500/50 blur-[160px] filter" />
 
+      <div className="glass-container rounded-lg bg-gradient-to-b from-slate-800 to-slate-900 p-4 md:rounded-xl">
+        <PlainLogo />
+      </div>
+
+      <div className="mt-8 max-w-xl text-balance text-5xl">
+        <PrismicText field={slice.primary.heading} />
+      </div>
+
+      <ButtonLink field={slice.primary.button_link} className="mt-6">
+        {slice.primary.button_text || "Learn More"}
+      </ButtonLink>
     </Bounded>
   );
 };
